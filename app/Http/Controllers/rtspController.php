f8904c698ed736f6b5c38f220927e45d17b22fee
Evilio Polo M <evilio.polo@omnia.com.co>
2018-09-20 14:00:47 -0500

    Se agrega clase helpers con funcionalidad islocalhost()

--- app/Http/Controllers/rtspController.php
+++ app/Http/Controllers/rtspController.php
@@ -30,7 +30,8 @@ class rtspController extends Controller
         $listas = array('rtsp://admin:vloxy123@192.168.1.108:554/cam/realmonitor?channel=1&subtype=0',
          'rtsp://admin:vloxy123@192.168.1.108:554/cam/realmonitor?channel=2&subtype=0', 
          'rtsp://admin:vloxy123@192.168.1.108:554/cam/realmonitor?channel=3&subtype=0',
-         'rtsp://admin:vloxy123@192.168.1.108:554/cam/realmonitor?channel=4&subtype=0');
+         'rtsp://admin:vloxy123@192.168.1.108:554/cam/realmonitor?channel=4&subtype=0',
+         isLocalhost());
 
         return view('viewcameras.indexgrid', compact('listas'));
     }
--- resources/views/viewcameras/indexgrid.blade.php
+++ resources/views/viewcameras/indexgrid.blade.php
@@ -1,8 +1,14 @@
 @extends('admin.layout')
 @section('content')    
+
+    <h1>{{isLocalhost()}}</h1>
+
     <div id="timeline" data-columns>
+
         @foreach($listas as $lista)
+        <h3>{!! $lista !!}</h3>
                 <div class="item embed-responsive embed-responsive-16by9">
+                        
                         <!-- <video id="test_video" controls autoplay class="embed-responsive-item">
                             <source src="{!! $lista !!}" type="application/x-rtsp">                
                         </video>             
